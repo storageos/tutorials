a33f0d4ef90862b950352069de5e3fbcdb8d54ed
Cheryl Hung <cheryl.hung@storageos.com>
2017-09-01 16:47:03 +0100

    Remove host name overrise

--- storageos-sandbox/000-install-storageos.sh
+++ storageos-sandbox/000-install-storageos.sh
@@ -3,7 +3,6 @@ export STORAGEOS_USERNAME=storageos STORAGEOS_PASSWORD=storageos STORAGEOS_HOST=
 CLUSTER_ID=$(storageos cluster create --size 3)
 
 docker -H host01:2345 run -d --name storageos \
-    -e HOSTNAME=storage-01 \
     -e ADVERTISE_IP=[[HOST_IP]] \
     -e CLUSTER_ID=$CLUSTER_ID \
     --net=host \
@@ -16,7 +15,6 @@ docker -H host01:2345 run -d --name storageos \
     storageos/node server
 
 docker -H host02:2345 run -d --name storageos \
-    -e HOSTNAME=storage-02 \
     -e ADVERTISE_IP=[[HOST2_IP]] \
     -e CLUSTER_ID=$CLUSTER_ID \
     --net=host \
@@ -29,7 +27,6 @@ docker -H host02:2345 run -d --name storageos \
     storageos/node server
 
 docker -H host03:2345 run -d --name storageos \
-    -e HOSTNAME=storage-03 \
     -e ADVERTISE_IP=[[HOST3_IP]] \
     -e CLUSTER_ID=$CLUSTER_ID \
     --net=host \
