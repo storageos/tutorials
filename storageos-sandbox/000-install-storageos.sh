2387b1b7a6221fadbbef16997bda6a0554bb0c07
Cheryl Hung <cheryl.hung@storageos.com>
2017-09-01 16:23:08 +0100

    Updated hostnames

--- storageos-sandbox/000-install-storageos.sh
+++ storageos-sandbox/000-install-storageos.sh
@@ -1,24 +1,11 @@
 export STORAGEOS_USERNAME=storageos STORAGEOS_PASSWORD=storageos STORAGEOS_HOST=[[HOST_IP]]
 
-TOKEN=$(storageos cluster create --size 3)
+CLUSTER_ID=$(storageos cluster create --size 3)
 
-docker -H host01:2345 run -d --name storageos \
+docker -H storage-01:2345 run -d --name storageos \
     -e HOSTNAME=storage-01 \
     -e ADVERTISE_IP=[[HOST_IP]] \
-    -e CLUSTER_ID=$TOKEN \
-    --net=host \
-    --pid=host \
-    --privileged \
-    --cap-add SYS_ADMIN \
-    --device /dev/fuse \
-    -v /var/lib/storageos:/var/lib/storageos:rshared \
-    -v /run/docker/plugins:/run/docker/plugins \
-    storageos/node server}}
-
-docker -H host02:2345 run -d --name storageos \
-    -e HOSTNAME=storage-02 \
-    -e ADVERTISE_IP=[[HOST2_IP]] \
-    -e CLUSTER_ID=$TOKEN \
+    -e CLUSTER_ID=$CLUSTER_ID \
     --net=host \
     --pid=host \
     --privileged \
@@ -28,10 +15,23 @@ docker -H host02:2345 run -d --name storageos \
     -v /run/docker/plugins:/run/docker/plugins \
     storageos/node server
 
-docker -H host03:2345 run -d --name storageos \
+docker -H storage-02:2345 run -d --name storageos \
+    -e HOSTNAME=storage-02 \
+    -e ADVERTISE_IP=[[HOST2_IP]] \
+    -e CLUSTER_ID=$CLUSTER_ID \
+    --net=host \
+    --pid=host \
+    --privileged \
+    --cap-add SYS_ADMIN \
+    --device /dev/fuse \
+    -v /var/lib/storageos:/var/lib/storageos:rshared \
+    -v /run/docker/plugins:/run/docker/plugins \
+    storageos/node server
+
+docker -H storage-03:2345 run -d --name storageos \
     -e HOSTNAME=storage-03 \
     -e ADVERTISE_IP=[[HOST3_IP]] \
-    -e CLUSTER_ID=$TOKEN \
+    -e CLUSTER_ID=$CLUSTER_ID \
     --net=host \
     --pid=host \
     --privileged \
