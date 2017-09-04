37ffbcddda533be6430d31c4cc90f492509e110e
Simon Croome <simon.croome@storageos.com>
2017-09-04 16:43:15 +0100

    Set hostname

--- storageos-sandbox/000-install-storageos.sh
+++ storageos-sandbox/000-install-storageos.sh
@@ -3,6 +3,7 @@ export STORAGEOS_USERNAME=storageos STORAGEOS_PASSWORD=storageos STORAGEOS_HOST=
 CLUSTER_ID=$(storageos cluster create --size 3)
 
 docker -H host01:2345 run -d --name storageos \
+    -e HOSTNAME=host01 \
     -e ADVERTISE_IP=[[HOST_IP]] \
     -e CLUSTER_ID=$CLUSTER_ID \
     --net=host \
@@ -15,6 +16,7 @@ docker -H host01:2345 run -d --name storageos \
     storageos/node server
 
 docker -H host02:2345 run -d --name storageos \
+    -e HOSTNAME=host02 \
     -e ADVERTISE_IP=[[HOST2_IP]] \
     -e CLUSTER_ID=$CLUSTER_ID \
     --net=host \
@@ -27,6 +29,7 @@ docker -H host02:2345 run -d --name storageos \
     storageos/node server
 
 docker -H host03:2345 run -d --name storageos \
+    -e HOSTNAME=host03 \
     -e ADVERTISE_IP=[[HOST3_IP]] \
     -e CLUSTER_ID=$CLUSTER_ID \
     --net=host \
