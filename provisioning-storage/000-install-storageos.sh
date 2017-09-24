6945fcd1c5f14abb8efa9bf17c906c646dd6bebf
Ben Hall <ben@benhall.me.uk>
2017-09-24 10:50:49 +0100

    Host01 can connect to other hosts directly

--- provisioning-storage/000-install-storageos.sh
+++ provisioning-storage/000-install-storageos.sh
@@ -6,3 +6,6 @@ docker -H host01:2345 run -d --name storageos -e HOSTNAME=host01 -e ADVERTISE_IP
 docker -H host02:2345 run -d --name storageos -e HOSTNAME=host02 -e ADVERTISE_IP=[[HOST2_IP]] -e CLUSTER_ID=$CLUSTER_ID --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins storageos/node:0.8.1 server
 
 docker -H host03:2345 run -d --name storageos -e HOSTNAME=host03 -e ADVERTISE_IP=[[HOST3_IP]] -e CLUSTER_ID=$CLUSTER_ID --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins storageos/node:0.8.1 server
+
+scp -p ~/.ssh/id_rsa root@host01:/root/.ssh
+ssh root@host01 "echo [[HOST2_IP]] host02 >> /etc/hosts && echo [[HOST3_IP]] host03 >> /etc/hosts"
