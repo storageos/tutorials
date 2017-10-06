ef26919814f19129135bc973fcf4e4da71e44939
Cheryl Hung <cheryl.hung@storageos.com>
2017-10-06 14:09:49 +0100

    Remove STORAGEOS_ env vars as it's now set in the base image

--- high-availability/000-install-storageos.sh
+++ high-availability/000-install-storageos.sh
@@ -1,4 +1,3 @@
-export STORAGEOS_USERNAME=storageos STORAGEOS_PASSWORD=storageos STORAGEOS_HOST=[[HOST_IP]]
 CLUSTER_ID=$(ssh root@host01 "storageos cluster create --size 3")
 
 docker -H host01:2345 run -d --name storageos -e HOSTNAME=host01 -e ADVERTISE_IP=[[HOST_IP]] -e CLUSTER_ID=$CLUSTER_ID --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins storageos/node:0.8.1 server
--- provisioning-storage/000-verify-healthy.sh
+++ provisioning-storage/000-verify-healthy.sh
@@ -1,2 +1,1 @@
-export STORAGEOS_USERNAME=storageos STORAGEOS_PASSWORD=storageos STORAGEOS_HOST=localhost
 for i in {1..20}; do /opt/verify-healthy.sh && break || sleep 1; done
