4680422ebbb504b905ee97a4e4c172c1be3f950f
Cheryl Hung <cheryl.hung@storageos.com>
2017-11-24 15:06:40 +0000

    Revert back to node container to allow for installation on remote hosts

--- docker-sandbox/000-install-storageos.sh
+++ docker-sandbox/000-install-storageos.sh
@@ -1,8 +1,10 @@
 CLUSTER_ID=$(ssh root@host01 "storageos cluster create --size 3")
 
-docker -H host01:2345 plugin install --grant-all-permissions --alias storageos storageos/plugin:0.9.0 ADVERTISE_IP=[[HOST_IP]] JOIN=$CLUSTER_ID
-docker -H host02:2345 plugin install --grant-all-permissions --alias storageos storageos/plugin:0.9.0 ADVERTISE_IP=[[HOST2_IP]] JOIN=$CLUSTER_ID
-docker -H host03:2345 plugin install --grant-all-permissions--alias storageos storageos/plugin:0.9.0 ADVERTISE_IP=[[HOST3_IP]] JOIN=$CLUSTER_ID
+docker -H host01:2345 run -d --name storageos -e HOSTNAME=host01 -e ADVERTISE_IP=[[HOST_IP]] -e CLUSTER_ID=$CLUSTER_ID --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins storageos/node:0.9.0 server
+
+docker -H host02:2345 run -d --name storageos -e HOSTNAME=host02 -e ADVERTISE_IP=[[HOST2_IP]] -e CLUSTER_ID=$CLUSTER_ID --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins storageos/node:0.9.0 server
+
+docker -H host03:2345 run -d --name storageos -e HOSTNAME=host03 -e ADVERTISE_IP=[[HOST3_IP]] -e CLUSTER_ID=$CLUSTER_ID --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins storageos/node:0.9.0 server
 
 scp -p ~/.ssh/id_rsa root@host01:/root/.ssh
 ssh root@host01 "echo [[HOST2_IP]] host02 >> /etc/hosts && echo [[HOST3_IP]] host03 >> /etc/hosts"
