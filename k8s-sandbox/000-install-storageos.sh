b37df9af8fb2a7034a35c91a30b620755604230f
Cheryl Hung <cheryl.hung@storageos.com>
2017-11-29 17:53:28 +0000

    ssh into first host

--- k8s-sandbox/000-install-storageos.sh
+++ k8s-sandbox/000-install-storageos.sh
@@ -1,2 +1,1 @@
-APIADDRESS=$(echo -n "tcp:/[[HOST2_IP]]:5705" | base64)
-sed -i 's/REPLACE/$APIADDRESS/g' storageos-secret.yaml
+ssh root@host01 "APIADDRESS=$(echo -n "tcp:/[[HOST2_IP]]:5705" | base64); sed -i 's/REPLACE/$APIADDRESS/g' storageos-secret.yaml"
