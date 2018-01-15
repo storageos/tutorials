9aedae43201be69b8d5c45914d58426266614e1d
Cheryl Hung <cheryl.hung@storageos.com>
2018-01-15 16:19:07 +0000

    Run command last

--- k8s-sandbox/000-install-storageos.sh
+++ k8s-sandbox/000-install-storageos.sh
@@ -1,4 +1,4 @@
 launch.sh
-docker -H host01:2345 run -d --name storageos -e HOSTNAME=host01 -e ADVERTISE_IP=[[HOST2_IP]] -e JOIN=[[HOST2_IP]] --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins storageos/node:0.9.2 server
 scp -p ~/.ssh/id_rsa root@host01:/root/.ssh
 ssh root@master "echo [[HOST2_IP]] node01 >> /etc/hosts"
+docker -H host01:2345 run -d --name storageos -e HOSTNAME=host01 -e ADVERTISE_IP=[[HOST2_IP]] -e JOIN=[[HOST2_IP]] --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins storageos/node:0.9.2 server
