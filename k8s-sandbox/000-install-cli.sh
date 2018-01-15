5c0586633d5f7d5d844267b150c8ef27617128c4
Cheryl Hung <cheryl.hung@storageos.com>
2018-01-15 14:36:05 +0000

    Move CLI install into background

--- k8s-sandbox/000-install-cli.sh
+++
@@ -1,1 +0,0 @@
-$(ssh root@host01 "curl -sSLo storageos https://github.com/storageos/go-cli/releases/download/0.9.3/storageos_linux_amd64 && chmod +x storageos && sudo mv storageos /usr/local/bin/; export STORAGEOS_USERNAME=storageos STORAGEOS_PASSWORD=storageos STORAGEOS_HOST=[[HOST2_IP]]")
--- k8s-sandbox/000-install-storageos.sh
+++ k8s-sandbox/000-install-storageos.sh
@@ -1,1 +1,2 @@
 launch.sh
+$(ssh root@host01 "curl -sSLo storageos https://github.com/storageos/go-cli/releases/download/0.9.3/storageos_linux_amd64 && chmod +x storageos && sudo mv storageos /usr/local/bin/; export STORAGEOS_USERNAME=storageos STORAGEOS_PASSWORD=storageos STORAGEOS_HOST=[[HOST_IP]]")
