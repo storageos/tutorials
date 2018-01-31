401f72f7221ea7fb40e08dfca6ac5e69fbcb72fe
Cheryl Hung <cheryl.hung@storageos.com>
2018-01-31 13:43:31 +0000

    Check node01 for health

--- k8s-sandbox/assets/verify-healthy.sh
+++ k8s-sandbox/assets/verify-healthy.sh
@@ -1,5 +1,5 @@
 echo 'Waiting for StorageOS to initialise... this will take a minute.'
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://host02:5705/v1/health)" != "200" ]]; do
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://node01:5705/v1/health)" != "200" ]]; do
   printf '.'
   sleep 5;
 done
