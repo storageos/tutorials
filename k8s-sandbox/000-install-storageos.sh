5cdbe9644f13d0c4d7409bb7c1dfbe2a8a0ce4af
Cheryl Hung <cheryl.hung@storageos.com>
2018-01-31 12:52:55 +0000

    Fixing ip addresses agin

--- k8s-sandbox/000-install-storageos.sh
+++ k8s-sandbox/000-install-storageos.sh
@@ -3,4 +3,4 @@ docker -H [[HOST2_IP]]:2345 run -d --name storageos -e HOSTNAME=node01 -e ADVERT
 curl -sSLo storageos https://github.com/storageos/go-cli/releases/download/0.9.2/storageos_linux_amd64 && chmod +x storageos && sudo mv storageos /usr/local/bin/
 
 scp -p ~/.ssh/id_rsa root@node01:/root/.ssh
-$(ssh root@node01] "export STORAGEOS_USERNAME=storageos STORAGEOS_PASSWORD=storageos STORAGEOS_HOST=[[HOST2_IP]]")
+$(ssh root@node01 "export STORAGEOS_USERNAME=storageos STORAGEOS_PASSWORD=storageos STORAGEOS_HOST=[[HOST2_IP]]")
