0958edc5b89f1bfd8cd37e173f68b9f034ec79a4
Cheryl Hung <cheryl.hung@storageos.com>
2018-01-31 12:37:27 +0000

    Fix Kubernetes sandbox tutorial by installing the container

--- k8s-sandbox/000-install-cli.sh
+++ k8s-sandbox/000-install-cli.sh
@@ -1,1 +1,1 @@
-$(ssh root@host01 "curl -sSLo storageos https://github.com/storageos/go-cli/releases/download/0.9.2/storageos_linux_amd64 && chmod +x storageos && sudo mv storageos /usr/local/bin/")
+$(ssh root@master "curl -sSLo storageos https://github.com/storageos/go-cli/releases/download/0.9.2/storageos_linux_amd64 && chmod +x storageos && sudo mv storageos /usr/local/bin/")
