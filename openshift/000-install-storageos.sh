28994b235c0aaf3d0e582cf17c7c5d8502ad7b6c
Cheryl Hung <cheryl.hung@storageos.com>
2018-02-28 10:15:42 +0000

    Remove OpenShift for now

--- openshift/000-install-storageos.sh
+++
@@ -1,3 +0,0 @@
-~/.launch.sh
-docker -H [[HOST_IP]]:2345 run -d --name storageos -e HOSTNAME=node01 -e ADVERTISE_IP=[[HOST_IP]] -e JOIN=[[HOST_IP]] --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins storageos/node:0.10.0 server
-curl -sSLo storageos https://github.com/storageos/go-cli/releases/download/0.9.2/storageos_linux_amd64 && chmod +x storageos && sudo mv storageos /usr/local/bin/
--- openshift/000-introduction.md
+++
@@ -1,1 +0,0 @@
-This sandbox environment is a fully configured one node OpenShift StorageOS cluster for hands on testing.
