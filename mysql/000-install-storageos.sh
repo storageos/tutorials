44201a4e77e9efcf5b4488d8c4828552e9f33767
StellaEngle <stella.engle@hotmail.com>
2019-10-16 15:11:36 +0100

    Change MySQL to use Kubernetes

--- mysql/000-install-storageos.sh
+++ mysql/000-install-storageos.sh
@@ -1,3 +1,3 @@
-if ssh root@host01 'ls /opt/bootstrap.sh'; then echo "bootstrap.sh present";else echo "/opt/bootstrap.sh not avaliable on host01"; fi
-if ssh root@host01 '/opt/bootstrap.sh'; then echo "Bootstrap complete"; fi
-scp -p ~/.ssh/id_rsa root@host01:/root/.ssh
+launch.sh
+curl -sSLo storageos https://github.com/storageos/go-cli/releases/download/1.2.2/storageos_linux_amd64 && chmod +x storageos && sudo mv storageos /usr/local/bin/
+ssh  -o "StrictHostKeyChecking=no" root@[[HOST2_IP]] "docker run --name enable_lio --privileged --rm --cap-add=SYS_ADMIN -v /lib/modules:/lib/modules -v /sys:/sys:rshared storageos/init:0.2;docker -H [[HOST2_IP]]:2345 run -d --name storageos -e DISABLE_TELEMETRY=true -e HOSTNAME=node01  -e ADVERTISE_IP=[[HOST2_IP]] -e JOIN=[[HOST2_IP]] --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /sys:/sys -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins storageos/node:1.4.0 server"
--- mysql/000-introduction.md
+++ mysql/000-introduction.md
@@ -1,1 +1,3 @@
-In this tutorial, you will learn how to use MySQL with StorageOS and configure volumes for high availability.
+This sandbox environment is a fully configured 3 nodes Kubernetes cluster.
+
+In this tutorial, you will install StorageOS and MySQL, create a StorageClass and use it to claim a persistent volume.
