c5751da91cf1cbd468760749056a0e47d13b1231
StellaEngle <stella.engle@hotmail.com>
2019-10-17 15:20:48 +0100

    CleanUp

--- mysql/000-install-storageos.sh
+++ mysql/000-install-storageos.sh
@@ -4,17 +4,8 @@ curl -sSLo storageos https://github.com/storageos/go-cli/releases/download/1.2.2
 ssh  -o "StrictHostKeyChecking=no" root@[[HOST1_IP]] "docker run --name enable_lio --privileged  \
 --rm --cap-add=SYS_ADMIN -v /lib/modules:/lib/modules  \
 -v /sys:/sys:rshared storageos/init:0.2; \
-docker -H [[HOST1_IP]]:2345 run -d --name storageos1 -e DISABLE_TELEMETRY=true \
+docker -H [[HOST1_IP]]:2345 run -d --name storageos -e DISABLE_TELEMETRY=true \
 -e HOSTNAME=node01  -e ADVERTISE_IP=[[HOST1_IP]] -e JOIN=[[HOST1_IP]] \
 --net=host --pid=host --privileged --cap-add SYS_ADMIN \
 --device /dev/fuse -v /sys:/sys -v /var/lib/storageos:/var/lib/storageos:rshared \
 -v /run/docker/plugins:/run/docker/plugins storageos/node:1.4.0 server"
-
-# ssh  -o "StrictHostKeyChecking=no" root@[[HOST2_IP]] "docker run --name enable_lio --privileged  \
-# --rm --cap-add=SYS_ADMIN -v /lib/modules:/lib/modules  \
-# -v /sys:/sys:rshared storageos/init:0.2; \
-# docker -H [[HOST2_IP]]:2345 run -d --name storageos -e DISABLE_TELEMETRY=true \
-# -e HOSTNAME=node02  -e ADVERTISE_IP=[[HOST2_IP]] -e JOIN=[[HOST1_IP]] \
-# --net=host --pid=host --privileged --cap-add SYS_ADMIN \
-# --device /dev/fuse -v /sys:/sys -v /var/lib/storageos:/var/lib/storageos:rshared \
-# -v /run/docker/plugins:/run/docker/plugins storageos/node:1.4.0 server"
