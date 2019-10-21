5eda5056dbb048e5aa87cf7ed313721c9b037e0f
StellaEngle <stella.engle@hotmail.com>
2019-10-21 13:04:55 +0100

    Clean up scenarios and leave in the Kubernetes and Postgress on docker for now

--- multitenancy/000-install-storageos.sh
+++
@@ -1,3 +0,0 @@
-if ssh root@host01 'ls /opt/bootstrap.sh'; then echo "bootstrap.sh present";else echo "/opt/bootstrap.sh not avaliable on host01"; fi
-if ssh root@host01 '/opt/bootstrap.sh'; then echo "Bootstrap complete"; fi
-scp -p ~/.ssh/id_rsa root@host01:/root/.ssh
--- multitenancy/assets/verify-healthy.sh
+++
@@ -1,8 +0,0 @@
-echo 'Waiting for StorageOS to initialise... this will take a minute.'
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:5705/v1/namespaces -u storageos:storageos)" != "200" ]]; do
-  printf '.'
-  sleep 5;
-done
-echo ''
-echo 'StorageOS is ready on Host01!'
-
