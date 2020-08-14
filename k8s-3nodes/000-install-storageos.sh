b6dabf0a9b4471703507e5d31f3bc28e51888200
Ferran <ferran.castell@storageos.com>
2020-08-14 12:04:43 +0100

    Remove obsolete scenarios

--- k8s-3nodes/000-install-storageos.sh
+++
@@ -1,8 +0,0 @@
-echo "Setting up the environment, please wait until you see the prompt."
-launch.sh
-curl -sSLo storageos https://github.com/storageos/go-cli/releases/download/1.2.2/storageos_linux_amd64 && chmod +x storageos && sudo mv storageos /usr/local/bin/
-# Use environment variables
-export STORAGEOS_USERNAME=storageos STORAGEOS_PASSWORD=storageos
-
-# Access StorageOS running on a remote host by providing the IP address
-export STORAGEOS_HOST=[[HOST2_IP]]
--- k8s-3nodes/000-introduction.md
+++
@@ -1,3 +0,0 @@
-This sandbox environment is a fully configured three node Kubernetes cluster.
-
-In this tutorial, you will install StorageOS, create a StorageClass and use it to claim a persistent volume.
