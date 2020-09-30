cc54b2c51f28572b79d8a8004c6ae35ac9d77abd
Tim Benger <tim.benger@storageos.com>
2020-09-30 16:02:59 +0100

    move position of clear command

--- influxdb/verify-healthy.sh
+++ influxdb/verify-healthy.sh
@@ -5,15 +5,15 @@ echo "Setting up Kubernetes, ETCD and StorageOS, please wait until you see the p
 declare -A hosts=( ["host01"]="node01" ["host02"]="node02" ["host03"]="node03")
 
 for host in ${!hosts[@]}; do
-    echo "Waiting for StorageOS to initialise on $host... this will take around 2 minutes."
 
 #Use the nodes endpoint rather than health because it takes longer for nodes
 #endpoint to come up and it seems to more accurately reflect when the cluster is
 #ready
+    clear;
     while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://${hosts[$host]}:5705 -u storageos:storageos)" != "200" ]]; do
-      printf '...'
+      clear
+      printf 'Waiting for storageOS to initialise on $host. This will take around 2 minutes...'
       sleep 5;
-      clear;
     done
 
     printf "\nStorageOS is ready on $host\n"
