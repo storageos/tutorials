65dc0a477a4b43d5aa4123bb6b0f79d4a24469d5
Tim Benger <tim.benger@storageos.com>
2020-09-30 17:19:13 +0100

    Remove verbose test stage

--- influxdb/verify-healthy.sh
+++ influxdb/verify-healthy.sh
@@ -2,20 +2,20 @@
 
 echo "Setting up Kubernetes, ETCD and StorageOS, please wait until you see the prompt. This takes around 2 minutes."
 
-declare -A hosts=( ["host01"]="node01" ["host02"]="node02" ["host03"]="node03")
+# declare -A hosts=( ["host01"]="node01" ["host02"]="node02" ["host03"]="node03")
 
-for host in ${!hosts[@]}; do
+# for host in ${!hosts[@]}; do
 
-#Use the nodes endpoint rather than health because it takes longer for nodes
-#endpoint to come up and it seems to more accurately reflect when the cluster is
-#ready
-    clear;
-    while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://${hosts[$host]}:5705 -u storageos:storageos)" != "200" ]]; do
-      clear
-      printf 'Waiting for storageOS to initialise on %s. This will take around 2 minutes...' $host
-      sleep 5;
-    done
+# #Use the nodes endpoint rather than health because it takes longer for nodes
+# #endpoint to come up and it seems to more accurately reflect when the cluster is
+# #ready
+#     clear;
+#     while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://${hosts[$host]}:5705 -u storageos:storageos)" != "200" ]]; do
+#       clear
+#       printf 'Waiting for storageOS to initialise on all hosts. This will take around 2 minutes...' 
+#       sleep 5;
+#     done
 
-    printf "\nStorageOS is ready on $host\n"
+#     printf "\nStorageOS is ready on $host\n"
 
-done
+# done
